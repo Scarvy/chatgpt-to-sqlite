c481ada86af2c7c9d4a6427a98a771ab96d67cd1
Scott Carvalho <scottcarvalho71@gmail.com>
2024-02-19 18:58:56 -0500

    Add 'export' command

--- chatgpt_to_sqlite/cli.py
+++ chatgpt_to_sqlite/cli.py
@@ -1,21 +1,59 @@
+import json
+
 import click
+import sqlite_utils
+from click_default_group import DefaultGroup
+from funcy import omit
+
+from chatgpt_to_sqlite import utils
 
 
-@click.group()
+@click.group(cls=DefaultGroup, default="export", default_if_no_args=True)
 @click.version_option()
 def cli():
     "Import ChatGPT conversations into a SQLite database"
 
 
-@cli.command(name="command")
+@cli.command(name="export")
 @click.argument(
-    "example"
+    "filename",
+    type=click.File(),
+)
+@click.argument(
+    "db_path",
+    type=click.Path(file_okay=True, dir_okay=False, allow_dash=False),
+    required=True,
 )
 @click.option(
-    "-o",
-    "--option",
-    help="An example option",
+    "-n",
+    "--num",
+    type=int,
+    default=None,
+    help="Number of conversations to load",
 )
-def first_command(example, option):
-    "Command description goes here"
-    click.echo("Here is some output")
+def export(filename, db_path, num):
+    """Import ChatGPT conversations into a SQlite database."""
+    data = json.load(filename)[:num] if num else json.load(filename)
+    click.echo(f"Loaded {len(data)} conversations.")
+
+    db = sqlite_utils.Database(db_path)
+
+    conversations = utils.load_documents(data)
+
+    # All Chat details except messages
+    convo_details = [omit(convo, "messages") for convo in conversations]
+
+    chatgpt_table = db.table("conversations")
+    chatgpt_table.upsert_all(convo_details, pk="chat_id")
+
+    # Store all chat messages
+    all_messages = []
+    for convo in conversations:
+        messages = convo["messages"]
+        for message in messages:
+            all_messages.append(message)
+
+    # Build messages tables
+    messages_table = db.table("messages")
+    messages_table.upsert_all(messages, pk="message_id")
+    messages_table.add_foreign_key("chat_id", "conversations", "chat_id", ignore=True)
---
+++ chatgpt_to_sqlite/utils.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from typing import Optional
+
+
+def convert_timestamps(timestamp: float | None) -> datetime | None:
+    if not timestamp:
+        return None
+    return datetime.fromtimestamp(timestamp)
+
+
+# source https://github.com/duarteocarmo/mistral-doc/blob/1f909bb4e23c4ae487890072192722c2e86da1f3/process_gpt_export.py#L16
+def concatenate_rows(message: dict, chat_id: str) -> Optional[dict]:
+    if not message:
+        return None
+
+    # sender (user, assistant, and system)
+    sender = message["author"]["role"] if message["author"] else "unknown"
+
+    if "parts" not in message["content"]:
+        return None
+
+    metadata = message.get("metadata", {})
+    is_user_system_message = metadata.get("is_user_system_message", False)
+
+    # System message
+    if is_user_system_message is True:
+        user_about_message = metadata.get("user_context_message_data", "").get(
+            "about_user_message", ""
+        )
+        about_model_message = metadata.get("user_context_message_data", "").get(
+            "about_model_message", ""
+        )
+        total_system_message = f"ABOUT YOU:\n{about_model_message}\n\nABOUT YOUR USER:\n{user_about_message}\n\nFIRST MESSAGE FROM THE USER:\n\n"
+        return {
+            "message_id": message["id"],
+            "sender": "system",
+            "create_time": convert_timestamps(message["create_time"]),
+            "status": message["status"],
+            "weight": message["weight"],
+            "text": total_system_message,
+            "model": None,
+            "chat_id": chat_id,
+        }
+
+    # User and assistant messages
+    text = message["content"]["parts"][0]
+
+    if text == "":
+        return None
+
+    model = message["metadata"].get("model_slug")
+
+    return {
+        "message_id": message["id"],
+        "sender": sender,
+        "create_time": convert_timestamps(message["create_time"]),
+        "status": message["status"],
+        "weight": message["weight"],
+        "text": text,
+        "model": model,
+        "chat_id": chat_id,
+    }
+
+
+# source: https://github.com/duarteocarmo/mistral-doc/blob/1f909bb4e23c4ae487890072192722c2e86da1f3/process_gpt_export.py#L44
+def load_documents(data: dict) -> list[list[dict]]:
+    documents = []
+    for d in data:
+        messages = d["mapping"]
+        messages = [
+            concatenate_rows(messages[key]["message"], d["id"])
+            for _, key in enumerate(messages)
+        ]
+        messages = [message for message in messages if message]
+        document = {
+            "chat_id": d["id"],
+            "title": d["title"],
+            "create_time": convert_timestamps(d["create_time"]),
+            "update_time": convert_timestamps(d["update_time"]),
+            "messages": messages,
+        }
+        documents.append(document)
+    return documents
